ac422c607c54bbc7a8e0d0e55ff51e266c810f96
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-12-27 23:18:21 +1300

    Lorenz system

--- src/Main.hs
+++ src/Main.hs
@@ -5,27 +5,34 @@ import ODE.Problem
 import ODE.RungeKutta4
 
 
--- Damped Pendulum 
-pendulumDeriv :: U.Vector Double -> U.Vector Double
-pendulumDeriv state = U.fromList [v, - sin x - 0.01*v] where
-                        x   = state U.! 0
-                        v   = state U.! 1
+-- Lorenz system 
+lorenzDeriv :: U.Vector Double -> U.Vector Double
+lorenzDeriv state = U.fromList [xDot, yDot, zDot] where
+                        x = state U.! 0
+                        y = state U.! 1
+                        z = state U.! 2
+                        xDot = sigma * (y - x)
+                        yDot = x * (rho - z) - y
+                        zDot = x * y - beta * z
+                        sigma = 10.0
+                        beta  = 8 / 3
+                        rho   = 28.0
 
--- Pendulum problem
-pendulumProblem :: Problem
-pendulumProblem = Problem (U.fromList [1.0, 0.0]) pendulumDeriv
+-- Lorenz problem
+lorenzProblem :: Problem
+lorenzProblem = Problem (U.fromList [1.0, 0.0, 0.0]) lorenzDeriv
 
 -- Main action
 main :: IO ()
 main = do
   -- Run parameters
-  let dt     = 0.01
+  let dt     = 0.001
       tFinal = 100.0
       thin   = 10
       steps  = floor $ tFinal / dt
 
   -- Construct the solver
-  let solver = case (makeRK4Solver pendulumProblem dt) of
+  let solver = case (makeRK4Solver lorenzProblem dt) of
         Nothing -> error "Failed to create solver."
         Just s  -> s
 
