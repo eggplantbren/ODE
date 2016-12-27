78dba986bcf463d49ab2d5da6e7a4a5e1aa883d4
Brendon J. Brewer <brendon.brewer@gmail.com>
2016-12-27 20:40:56 +1300

    Bit of work

--- lib/ODE/Problem.hs
+++ lib/ODE/Problem.hs
@@ -4,6 +4,9 @@ import qualified Data.Vector.Unboxed as U
 
 data Problem = Problem
                {
+                 -- Initial conditions
+                 initialConditions :: U.Vector Double,
+
                  -- Derivative function
                  deriv :: U.Vector Double -> U.Vector Double
                }
--- lib/ODE/RungeKutta4.hs
+++ lib/ODE/RungeKutta4.hs
@@ -1,10 +1,33 @@
-module ODE.RungeKutta4 where
+module ODE.RungeKutta4
+  (makeRK4Solver)
+  where
 
+import qualified Data.Vector.Unboxed as U
 import ODE.Problem
 
+-- A type to represent a solver
 data RK4Solver = RK4Solver
                  {
-                   dt      :: Double,
-                   problem :: Problem
+                   problem      :: Problem,
+                   dt           :: Double,
+                   iteration    :: Int,
+                   currentState :: U.Vector Double
                  }
+
+-- Smart constructor
+makeRK4Solver :: Problem -> Double -> Maybe RK4Solver
+makeRK4Solver problemIn dtIn
+    | dtIn <= 0.0 = Nothing
+    | otherwise   = Just RK4Solver
+                    {
+                      problem      = problemIn,
+                      dt           = dtIn,
+                      iteration    = 0,
+                      currentState = initialConditions problemIn
+                    }
+
+-- Update
+update :: RK4Solver -> RK4Solver
+update solver = solver
+
 
